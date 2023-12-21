e4e49a72221b80c12b7cafce20aa3d4695ec4d56
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2023-12-20 22:03:19 -0800

    Clean up

--- src/routes/projects/new-project/utils.js
+++
@@ -1,7 +0,0 @@
-export const getFieldOnChange =
-  (fieldType, fieldKey, onChangeFn) =>
-    ({ detail: { selectedOption, value } }) =>
-      onChangeFn({
-        [fieldKey]: fieldType === "select" ? selectedOption : value,
-      })
-
